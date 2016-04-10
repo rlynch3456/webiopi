e9cf30f098c187f6a6170a1c8dd8776bd651245f
Rich Lynch <rlynch3456@yahoo.com>
2016-04-10 18:07:29 +0000

    initial commit

---
+++ rock_launch/python/script.py
@@ -0,0 +1,18 @@
+import webiopi
+ 
+GPIO = webiopi.GPIO
+ 
+Rocket = 17 # GPIO pin using BCM numbering
+ 
+ 
+# setup function is automatically called at WebIOPi startup
+def setup():
+    # set the GPIO used by the light to output
+    GPIO.setFunction(Rocket, GPIO.OUT)
+    GPIO.digitalWrite(Rocket, GPIO.LOW)
+# loop function is repeatedly called by WebIOPi
+def loop():
+ 
+ 
+    # gives CPU some time before looping again
+    webiopi.sleep(1)
